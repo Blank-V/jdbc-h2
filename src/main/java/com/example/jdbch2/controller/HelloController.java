740caf2b32b7a098c3a0ecf4cdb565c8a21b53e3
Blank-V <704310618@qq.com>
2020-02-14 21:34:23 +0800

    jdbc-h2 project index.html

--- src/main/java/com/example/jdbch2/controller/HelloController.java
+++ src/main/java/com/example/jdbch2/controller/HelloController.java
@@ -4,34 +4,40 @@ import com.example.jdbch2.Customer;
 import com.example.jdbch2.service.RunService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 
-@RestController
+@Controller
 
 public class HelloController {
 
     @Autowired
         private RunService runService;
 
-    @Value("${helloworld}")
-        private String hello;
-    @Resource
-        private Customer customer;
+//    @Value("${helloworld}")
+//        private String hello;
+//    @Resource
+//        private Customer customer;
 
-    @RequestMapping("/hello")
-    public String hello(){
-
-
-        return runService.writeRun();
+    @GetMapping("/greeting")
+    @ResponseBody
+    public String greeting(){
+        System.out.println("Hello");
+        return "greeting";
 
 //        customer.setFirstName("Job");
 //        customer.setLastName("mia");
 //        return hello +"<br/>"+
 //                "firstName: "+customer.getFirstName()+"<br/>"+
 //                "lastName: "+ customer.getLastName();
+    }
+    @GetMapping("/hi")
+    public String hi(@RequestParam(name="name",required = false,defaultValue = "World")String name, Model model){
+        model.addAttribute("name",name);
+        return "hi";
     }
 }
