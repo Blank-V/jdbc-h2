17ca55ed75668fd54c847f3a05f236fbe6219129
Blank-V <704310618@qq.com>
2020-02-15 01:29:34 +0800

    jdbc-h2 project index.html

--- src/main/java/com/example/jdbch2/controller/HelloController.java
+++ src/main/java/com/example/jdbch2/controller/HelloController.java
@@ -1,6 +1,8 @@
 package com.example.jdbch2.controller;
 
 import com.example.jdbch2.Customer;
+import com.example.jdbch2.dao.User;
+import com.example.jdbch2.dao.UserDao;
 import com.example.jdbch2.service.RunService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -10,6 +12,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.*;
 
 @Controller
 
@@ -22,9 +25,44 @@ public class HelloController {
 //        private String hello;
 //    @Resource
 //        private Customer customer;
+@RequestMapping(value = "/user/map", method = RequestMethod.GET)
+public String map(Model model) {
+    Map<String, User> allMembers = new HashMap<String,User>();
+    for (int x = 0; x < 10; x++) {
+        User vo = new User();
+        vo.setUid(101L + x);
+        vo.setName("赵四 - " + x);
+        vo.setAge(9);
+        vo.setSalary(99999.99);
+        vo.setBirthday(new Date());
+        allMembers.put("mldn-" + x, vo);
+    }
+    model.addAttribute("allUsers", allMembers);
+    return "user_map";
+}
 
+    @RequestMapping(value = "/user/list", method = RequestMethod.GET)
+    public String list(Model model) {
+        List<User> allMembers = new ArrayList<User>();
+        for (int x = 0; x < 10; x++) {
+            User vo = new User();
+            vo.setUid(101L + x);
+            vo.setName("赵四 - " + x);
+            vo.setAge(9);
+            vo.setSalary(99999.99);
+            vo.setBirthday(new Date());
+            allMembers.add(vo) ;
+        }
+        model.addAttribute("allUsers", allMembers);
+        return "user_list";
+    }
     @RequestMapping(value = "show" , method=RequestMethod.GET)
     public String show(Model model){
+        UserDao user = new UserDao();
+        user.setStartTime("Tom");
+        user.setEndTime(111);
+        user.setBirthDay(new Date());
+        model.addAttribute("user",user);
         model.addAttribute("uid","123456");
         model.addAttribute("name","Jerry");
         System.out.println("Hello");
--- src/main/java/com/example/jdbch2/dao/UserDao.java
+++ src/main/java/com/example/jdbch2/dao/UserDao.java
@@ -2,10 +2,21 @@ package com.example.jdbch2.dao;
 
 import org.springframework.stereotype.Repository;
 
+import java.util.Date;
+
 @Repository(value = "userDao")
 public class UserDao {
     private String startTime;
     private int endTime;
+    private Date birthDay;
+
+    public Date getBirthDay() {
+        return birthDay;
+    }
+
+    public void setBirthDay(Date birthDay) {
+        this.birthDay = birthDay;
+    }
 
     public String getStartTime() {
         return startTime;
